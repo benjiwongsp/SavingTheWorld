edd914d648197a6935f61240e4d01194574cf920
Siu Pong Wong <Benjiw@Sius-MacBook-Pro.local>
2017-05-01 15:38:25 +0800

    Initial Commit

---
+++ SavingTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SavingTheWorld
+//
+//  Created by Siu Pong Wong on 1/5/2017.
+//  Copyright © 2017 Siu Pong Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
